65e58bed4c3fcb65c5ba55e6550ab1dcb85c915e
Atlungstad Brenneri <camilla_repp@hotmail.com>
2021-06-03 14:00:34 +0200

    changes done

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Layout from './components/layout/Layout';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <Layout />
+    </div>
+  );
+}
+
+export default App;
